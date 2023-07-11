ac1f8df18bff30806b03e806772e3d2c13427e5b
valentimkoniarski <66980970+valentimkoniarski@users.noreply.github.com>
2023-07-11 10:03:18 -0300

    ajuste url do backend aws

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,4 +1,4 @@
 // export const API_URL = 'https://crud-products-back-end-production.up.railway.app/'
 //export const API_URL = 'http://localhost:8085/'
 
-export const API_URL = 'http://18.222.69.52:8085'
+export const API_URL = 'http://18.222.69.52:8085/'
